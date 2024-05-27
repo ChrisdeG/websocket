bcf4cff98bead6bb230f6ee4bf0fc714d4fecd13
Chris de Gelder <ChrisdeG@users.noreply.github.com>
2024-05-27 17:02:56 +0200

    Upgraded source library websocket
    Upgrade to websocket 1.0.35 and pluggable widgets tools 9.24

--- typings/WebsocketProps.d.ts
+++ typings/WebsocketProps.d.ts
@@ -1,10 +1,11 @@
 /**
  * This file was generated from Websocket.xml
  * WARNING: All changes made to this file will be overwritten
- * @author Mendix UI Content Team
+ * @author Mendix Widgets Framework Team
  */
 import { CSSProperties } from "react";
 import { ActionValue, EditableValue } from "mendix";
+import { Big } from "big.js";
 
 export interface WebsocketContainerProps {
     name: string;
@@ -12,20 +13,26 @@ export interface WebsocketContainerProps {
     style?: CSSProperties;
     tabIndex?: number;
     subscriptionAttribute: EditableValue<string>;
-    progressAttribute?: EditableValue<BigJs.Big>;
+    progressAttribute?: EditableValue<Big>;
     text1Attribute?: EditableValue<string>;
     text2Attribute?: EditableValue<string>;
     text3Attribute?: EditableValue<string>;
     booleanAttribute?: EditableValue<boolean>;
-    decimalAttribute?: EditableValue<BigJs.Big>;
+    decimalAttribute?: EditableValue<Big>;
     dateAttribute?: EditableValue<Date>;
     onUpdateAction?: ActionValue;
     path: string;
 }
 
 export interface WebsocketPreviewProps {
+    /**
+     * @deprecated Deprecated since version 9.18.0. Please use class property instead.
+     */
+    className: string;
     class: string;
     style: string;
+    styleObject?: CSSProperties;
+    readOnly: boolean;
     subscriptionAttribute: string;
     progressAttribute: string;
     text1Attribute: string;
